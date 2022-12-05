a5143039e60fde8f76cd1b40ccff0aaf17888d63
VN The Boss Baby <93357243+QuDaMyker@users.noreply.github.com>
2022-12-05 15:55:09 +0700

    commit

--- Music-Player-Project-IT008N13/Form1.Designer.cs
+++ Music-Player-Project-IT008N13/Form1.Designer.cs
@@ -29,37 +29,19 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.btnHome = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.btnMusicLibrary = new System.Windows.Forms.Button();
-            this.btnVideoLibrary = new System.Windows.Forms.Button();
-            this.btnPlayQueue = new System.Windows.Forms.Button();
-            this.btnPlaylists = new System.Windows.Forms.Button();
+            this.addMediaQueueForm1 = new Music_Player_Project_IT008N13.addMediaQueueForm();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Controls.Add(this.btnPlaylists);
-            this.panel1.Controls.Add(this.btnPlayQueue);
-            this.panel1.Controls.Add(this.btnVideoLibrary);
-            this.panel1.Controls.Add(this.btnMusicLibrary);
-            this.panel1.Controls.Add(this.btnHome);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(306, 834);
             this.panel1.TabIndex = 0;
-            // 
-            // btnHome
-            // 
-            this.btnHome.Location = new System.Drawing.Point(26, 71);
-            this.btnHome.Name = "btnHome";
-            this.btnHome.Size = new System.Drawing.Size(237, 50);
-            this.btnHome.TabIndex = 0;
-            this.btnHome.Text = "Home";
-            this.btnHome.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
@@ -69,46 +51,20 @@
             this.textBox1.Size = new System.Drawing.Size(237, 41);
             this.textBox1.TabIndex = 1;
             // 
-            // btnMusicLibrary
+            // addMediaQueueForm1
             // 
-            this.btnMusicLibrary.Location = new System.Drawing.Point(26, 127);
-            this.btnMusicLibrary.Name = "btnMusicLibrary";
-            this.btnMusicLibrary.Size = new System.Drawing.Size(237, 50);
-            this.btnMusicLibrary.TabIndex = 0;
-            this.btnMusicLibrary.Text = "Music Library";
-            this.btnMusicLibrary.UseVisualStyleBackColor = true;
-            // 
-            // btnVideoLibrary
-            // 
-            this.btnVideoLibrary.Location = new System.Drawing.Point(26, 183);
-            this.btnVideoLibrary.Name = "btnVideoLibrary";
-            this.btnVideoLibrary.Size = new System.Drawing.Size(237, 50);
-            this.btnVideoLibrary.TabIndex = 0;
-            this.btnVideoLibrary.Text = "Video Library";
-            this.btnVideoLibrary.UseVisualStyleBackColor = true;
-            // 
-            // btnPlayQueue
-            // 
-            this.btnPlayQueue.Location = new System.Drawing.Point(26, 239);
-            this.btnPlayQueue.Name = "btnPlayQueue";
-            this.btnPlayQueue.Size = new System.Drawing.Size(237, 50);
-            this.btnPlayQueue.TabIndex = 0;
-            this.btnPlayQueue.Text = "Play Queue";
-            this.btnPlayQueue.UseVisualStyleBackColor = true;
-            // 
-            // btnPlaylists
-            // 
-            this.btnPlaylists.Location = new System.Drawing.Point(26, 295);
-            this.btnPlaylists.Name = "btnPlaylists";
-            this.btnPlaylists.Size = new System.Drawing.Size(237, 50);
-            this.btnPlaylists.TabIndex = 0;
-            this.btnPlaylists.UseVisualStyleBackColor = true;
+            this.addMediaQueueForm1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.addMediaQueueForm1.Location = new System.Drawing.Point(312, 12);
+            this.addMediaQueueForm1.Name = "addMediaQueueForm1";
+            this.addMediaQueueForm1.Size = new System.Drawing.Size(1343, 704);
+            this.addMediaQueueForm1.TabIndex = 1;
             // 
             // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1667, 834);
+            this.Controls.Add(this.addMediaQueueForm1);
             this.Controls.Add(this.panel1);
             this.Name = "mainForm";
             this.Text = "Media Player Project";
@@ -121,12 +77,8 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button btnHome;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button btnPlayQueue;
-        private System.Windows.Forms.Button btnVideoLibrary;
-        private System.Windows.Forms.Button btnMusicLibrary;
-        private System.Windows.Forms.Button btnPlaylists;
+        private addMediaQueueForm addMediaQueueForm1;
     }
 }
 
---
+++ Music-Player-Project-IT008N13/addMediaQueueForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Music_Player_Project_IT008N13
+{
+    public partial class addMediaQueueForm : UserControl
+    {
+        public addMediaQueueForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
