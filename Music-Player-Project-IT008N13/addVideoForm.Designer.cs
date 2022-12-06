dd9ef3165b2f16613f73a119f440ba521066b861
VN The Boss Baby <93357243+QuDaMyker@users.noreply.github.com>
2022-12-06 23:01:08 +0700

    commmit

--- Music-Player-Project-IT008N13/addVideoForm.Designer.cs
+++ Music-Player-Project-IT008N13/addVideoForm.Designer.cs
@@ -28,19 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(addVideoForm));
+            this.labelNameVideo = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButtonAddFolder = new System.Windows.Forms.ToolStripButton();
+            this.panel1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // labelNameVideo
+            // 
+            this.labelNameVideo.AutoSize = true;
+            this.labelNameVideo.Font = new System.Drawing.Font("Forte", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNameVideo.Location = new System.Drawing.Point(28, 21);
+            this.labelNameVideo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelNameVideo.Name = "labelNameVideo";
+            this.labelNameVideo.Size = new System.Drawing.Size(180, 70);
+            this.labelNameVideo.TabIndex = 6;
+            this.labelNameVideo.Text = "Video";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.toolStrip1);
+            this.panel1.Location = new System.Drawing.Point(867, 21);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(126, 32);
+            this.panel1.TabIndex = 7;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButtonAddFolder});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(126, 28);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButtonAddFolder
+            // 
+            this.toolStripButtonAddFolder.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripButtonAddFolder.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonAddFolder.Image")));
+            this.toolStripButtonAddFolder.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonAddFolder.Name = "toolStripButtonAddFolder";
+            this.toolStripButtonAddFolder.Size = new System.Drawing.Size(114, 25);
+            this.toolStripButtonAddFolder.Text = "  Add Folder";
             // 
             // addVideoForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.labelNameVideo);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "addVideoForm";
-            this.Size = new System.Drawing.Size(1358, 797);
+            this.Size = new System.Drawing.Size(1018, 648);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelNameVideo;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButtonAddFolder;
     }
 }
